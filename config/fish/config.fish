8671dcad6657f46e8b3c537a7467c5a419230dc6
lePerdu <zdpeltzer@gmail.com>
2024-01-10 12:08:00 -0500

    Add fish config for cargo, ghcup, and opam

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,26 +10,40 @@ alias l="ls -lhv"
 set -x LESS -isR
 
 if command -q emacsclient
-    set -x EDITOR 'emacsclient -r'
+    set -gx EDITOR 'emacsclient -r'
     alias e 'emacsclient -r -n'
 else if command -q nvim
-    set -x EDITOR nvim
+    set -gx EDITOR nvim
     alias v nvim
 else if command -q vi
-    set -x EDITOR vi
+    set -gx EDITOR vi
     alias v nvim
 else
-    set -x EDITOR nano
+    set -gx EDITOR nano
 end
 
-set -x VISUAL $EDITOR
+set -gx VISUAL $EDITOR
 
 alias venv="python3 -m venv"
 alias pip="python3 -m pip"
 
-contains $HOME/.local/bin $PATH
-or set -gx PATH $PATH $HOME/.local/bin
+if not contains $HOME/.local/bin $PATH
+    set -gxp PATH $HOME/.local/bin
+end
 
+# Cargo
+if test -d ~/.cargo/bin and not contains $HOME/.cargo/bin $PATH
+    set -gxp PATH $HOME/.cargo/bin
+end
 
+# ghcup
+if test -d ~/.ghcup/bin and not contains $HOME/.ghcup/bin $PATH
+    set -gxp PATH $HOME/.cabal/bin $HOME/.ghcup/bin
+    set GHCUP_INSTALL_BASE_PREFIX $HOME
+end
 
-
+# opam
+if test -d ~/.opam
+    source ~/.opam/opam-init/init.fish >/dev/null 2>/dev/null
+    or true
+end
